f850eeeb270fa9d9b6d3b37eebc9c8c3cc36eb3a
iVampireSP.com <im@ivampiresp.com>
2024-10-18 23:40:52 +0800

    Update provider.go

--- internal/handler/grpc/provider.go
+++ internal/handler/grpc/provider.go
@@ -2,31 +2,46 @@ package grpc
 
 import (
 	"github.com/google/wire"
-	"go-template/internal/handler/grpc/documents"
-	"go-template/internal/handler/grpc/interceptor"
+	"leafdev.top/Leaf/leaf-library/internal/handler/grpc/documents"
+	"leafdev.top/Leaf/leaf-library/internal/handler/grpc/interceptor"
 )
+
+var ProviderSet = wire.NewSet(
+	interceptor.NewAuth,
+	interceptor.NewLogger,
+	documents.NewDocumentService,
+
+	NewInterceptor,
+	NewHandler,
+)
+
+func NewHandler(
+	documentService *documents.DocumentService,
+	interceptor2 *Interceptor,
+) *Handlers {
+	return &Handlers{
+		DocumentService: documentService,
+		Interceptor:     interceptor2,
+	}
+}
+
+type Handlers struct {
+	DocumentService *documents.DocumentService
+	Interceptor     *Interceptor
+}
 
 type Interceptor struct {
 	Auth   *interceptor.Auth
 	Logger *interceptor.Logger
 }
 
-func NewHandler(
-	documentService *documents.DocumentService,
-) *Handlers {
-	return &Handlers{
-		DocumentService: documentService,
+func NewInterceptor(
+	Auth *interceptor.Auth,
+	Logger *interceptor.Logger,
+) *Interceptor {
+	return &Interceptor{
+		Auth:   Auth,
+		Logger: Logger,
 	}
-}
 
-var ProviderSet = wire.NewSet(
-	documents.NewDocumentService,
-	interceptor.NewAuth,
-	interceptor.NewLogger,
-	NewHandler,
-)
-
-type Handlers struct {
-	DocumentService *documents.DocumentService
-	Interceptor     *Interceptor
 }
