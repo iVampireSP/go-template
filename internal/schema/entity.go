550ad205513e68dfc58bafe2be7639388bc5a52a
ivamp <im@ivampiresp.com>
2024-11-29 02:53:59 +0800

    增加 uint64 和 int64 的转换

--- internal/schema/entity.go
+++ internal/schema/entity.go
@@ -6,12 +6,18 @@ import (
 
 type EntityId uint
 
-//type EntityId int64
-
 func (i EntityId) String() string {
 	return strconv.FormatUint(uint64(i), 10)
-	//return strconv.FormatInt(int64(i), 10)
 }
+
 func (i EntityId) Uint() uint {
 	return uint(i)
+}
+
+func (i EntityId) Uint64() uint64 {
+	return uint64(i)
+}
+
+func (i EntityId) Int64() int64 {
+	return int64(i)
 }
